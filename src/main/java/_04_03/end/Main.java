082464996d23e0799167f36cb214cea39c5c3e2c
Jake Moffat <117701842+jakemoffat@users.noreply.github.com>
2023-06-12 19:18:27 +0000

    Vid04_03 Edits

--- src/main/java/_04_03/end/Main.java
+++ src/main/java/_04_03/end/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        // Is the same as numbers.forEach((number) -> NumberUtils.evenOrOdd(number));
         numbers.forEach(NumberUtils::evenOrOdd);
     }
 
